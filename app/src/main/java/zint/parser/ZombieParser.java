cf0fac8de2e0a37faa52716e6d7a644c049c9eef
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-12-01 15:09:39 +0100

    Created Parser

---
+++ app/src/main/java/zint/parser/ZombieParser.java
@@ -0,0 +1,12 @@
+package zint.parser;
+
+import zint.lexer.*;
+
+public class ZombieParser {
+
+	private final ZombieLexer lexer;
+
+	public ZombieParser(final ZombieLexer lexer) {
+		this.lexer = lexer;
+	}
+}
