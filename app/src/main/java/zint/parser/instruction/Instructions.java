16623bec888dafa4e0f33cf368978af8cdd6c6bf
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-12-01 16:02:27 +0100

    Added first Instruction

--- app/src/main/java/zint/parser/instruction/Instructions.java
+++ app/src/main/java/zint/parser/instruction/Instructions.java
@@ -3,11 +3,18 @@ package zint.parser.instruction;
 import zint.lexer.token.*;
 
 import java.util.*;
-import java.util.function.*;
+
+import static java.util.Map.*;
 
 public class Instructions {
 
-	private final Map<InstructionType, Predicate<List<Token>>> instructionMap = Map.ofEntries(
-
+	private final Map<InstructionType, TokenAcceptor> instructionMap = Map.ofEntries(
+		entry(InstructionType.ENTITY_DECLARATION, new TokenAcceptorBuilder()
+				.one(TokenType.ID)
+				.oneOrMore(TokenType.WHITESPACE)
+				.one(TokenType.IS_A)
+				.oneOrMore(TokenType.WHITESPACE)
+				.one(TokenType.ENTITY)
+				.build())
 	);
 }
