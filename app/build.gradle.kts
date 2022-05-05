d9fcf0a92340a1322f0a8661a331d40f829df28e
Ledmington <ledmington.dev@gmail.com>
2022-05-05 10:29:51 +0200

    Added checkstyle, pmd, jacoco, antlr

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,10 @@ version = "0.1.0"
 plugins {
     application
     java
+    checkstyle
+    pmd
+    jacoco
+    antlr
 }
 
 repositories {
@@ -11,13 +15,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 
     implementation("com.google.guava:guava:30.1.1-jre")
+
+    implementation("org.antlr:antlr4:4.10.1")
 }
 
-application {
-    mainClass.set("zint.Zint")
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.test {
@@ -28,5 +35,31 @@ tasks.create<JavaExec>("runOnConsole") {
     group = "run"
     standardInput = System.`in`
     classpath = project.sourceSets.main.get().runtimeClasspath
+    mainClass.set("zint.Zint")
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        html.stylesheet = resources.text.fromFile("config/xsl/checkstyle-custom.xsl")
+    }
+}
+
+checkstyle {
+    toolVersion = "8.15"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = true
+}
+
+pmd {
+    isIgnoreFailures = true
+}
+
+jacoco {
+    toolVersion = "0.8.2"
+}
+
+application {
     mainClass.set("zint.Zint")
 }
